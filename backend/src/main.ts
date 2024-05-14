87a1901e989dca417916b887852cedd948818d7f
roberto2002-03-12 <robertog-18@hotmail.com>
2024-05-14 12:37:41 -0500

    habilitar cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    // definir que rutas permitir en origin
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       // elimina los datos incorrectos
