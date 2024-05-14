15ff5334dde5c0e01ee72159affb9adf87713b70
roberto2002-03-12 <robertog-18@hotmail.com>
2024-05-14 13:02:20 -0500

    just delete

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,13 +6,6 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    // definir que rutas permitir en origin
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       // elimina los datos incorrectos
